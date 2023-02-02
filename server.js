24afed8d65e58d62baddc3f207edf8a5fa708e08
Jerry Victor <jerryvictor@jerrys-mbp.myfiosgateway.com>
2023-02-02 17:06:58 -0500

    established middleware

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db')
+const logger = require('./utils/logger')
 
 dotenv.config({ path: './config/config.env' })
 
@@ -9,6 +10,8 @@ connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 5001;
+
+app.use(logger)
 
 const server = app.listen(PORT, () => {
     console.log(`Server is listening on PORT ${PORT}`)
---
+++ utils/logger.js
@@ -0,0 +1,7 @@
+// MIDDLEWARE
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.protocol}://${req.get('host')} ${req.originalUrl}`)
+}
+
+module.exports = logger;
+
